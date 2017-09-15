f3dba24a9495dca9e5e1e526cb67a2d7a21bb203
Alexander <mrrandomgamerdkhd@gmail.com>
2017-09-15 21:43:19 +0200

    Added asset loader

---
+++ public/js/core/AssetLoader.js
@@ -0,0 +1,35 @@
+var AssetLoader = {
+  tileImages: [],
+  imgsToLoad: 0,
+
+  countLoadedImages: function () {
+    AssetLoader.imgsToLoad--
+    console.log('[DEBUG] Images left to load: '+ AssetLoader.imgsToLoad);
+    if (AssetLoader.imgsToLoad == 0) {
+      console.log('Starting Game');
+      startGame()
+    }
+  },
+
+  loadImage: function (imgVar, src) {
+    imgVar.onload = AssetLoader.countLoadedImages
+    imgVar.src = 'assets/'+src
+  },
+
+  loadTileImage: function (type, src) {
+    AssetLoader.tileImages[type] = document.createElement('img')
+    AssetLoader.loadImage(AssetLoader.tileImages[type], src)
+  },
+
+  loadImages: function (images) {
+    AssetLoader.imgsToLoad = images.length
+
+    for (var i = 0; i < AssetLoader.imgsToLoad; i++) {
+      if (images[i].variable != undefined) {
+        AssetLoader.loadImage(images[i].variable, images[i].src)
+      } else {
+        AssetLoader.loadTileImage(images[i].tileType, images[i].src)
+      }
+    }
+  }
+}
